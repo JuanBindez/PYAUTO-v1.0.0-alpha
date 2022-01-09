a0a8d77ba0942f667b7355cd256a3d79ffa25690
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-01-09 20:00:39 -0300

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,9 +1,14 @@
 #python3 auto.py v.02
 
-import pyautogui
-import time
-import pyautogui, sys
 import os
+try:
+    import pyautogui
+    import time
+    import pyautogui, sys
+except ModuleNotFoundError:
+    os.system("clear")
+    print("instale o pyautogui com o pip")
+
 
 def function01():
     print('Press Ctrl-C to quit.')
@@ -22,33 +27,39 @@ def function02():
     time.sleep(2)
     pyautogui.click(x=20, y=633)
     time.sleep(1)
-    pyautogui.moveTo(214, 147, 2, pyautogui.easeInQuad)
     pyautogui.click(x=214, y=147)
     time.sleep(1)
     pyautogui.write('ola', interval=0.25,) 
     time.sleep(1)
-    pyautogui.moveTo(679, 47, 2, pyautogui.easeInQuad)
     pyautogui.click(x=679, y=47)
     time.sleep(2)
-    pyautogui.moveTo(183, 386, 2, pyautogui.easeInQuad)
     pyautogui.click(x=183, y=386)
     time.sleep(2)
     #o codigo abaixo abre o terminal
     pyautogui.hotkey('ctrl', 'alt', 't')
     time.sleep(1)
-    pyautogui.moveTo(500, 300, 2, pyautogui.easeInQuad)
     pyautogui.click(x=500, y=300)
     time.sleep(1)
-    pyautogui.write('ping 10.0.0.1', interval=0.25,)
+    pyautogui.write('ping 191.7.178.229', interval=0.25,)
     pyautogui.press('enter')
 
 print("1 ver localização do mouse")
 print("2 para executar automação")
+
 x = int(input("digite um numero: "))
 
 if x == 1:
-    function01()
+    try:
+        function01()
+    except NameError:
+        os.system("clear")
+        print("instale o pyautogui com o pip")
+
 
 if x == 2:
-    function02()
+    try:
+        function02()
+    except NameError:
+        os.system("clear")
+        print("instale o pyautogui com o pip")
 
