427d2d948af39e393b1b282819dec73300fb4a58
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-03-10 12:04:43 -0300

    Update auto.py

--- auto.py
+++ auto.py
@@ -10,15 +10,15 @@ try:
         print("instale o pyautogui com o pip")
         
         
-class Color():
-    VERDE = '\033[92m'
-    VERMELHO = '\033[91m'
-    AMARELO = '\033[93m'
-    AZUL = '\033[1;34m'
-    MAGENTA = '\033[1;35m'
-    VERDE_CLARO = '\033[1;92m'
-    NEGRITO = '\033[;1m'
-    RESET = '\033[0m'
+    class Color():
+        VERDE = '\033[92m'
+        VERMELHO = '\033[91m'
+        AMARELO = '\033[93m'
+        AZUL = '\033[1;34m'
+        MAGENTA = '\033[1;35m'
+        VERDE_CLARO = '\033[1;92m'
+        NEGRITO = '\033[;1m'
+        RESET = '\033[0m'
 
 
     def function01():
